7d1de880a6fa0a1344e954ad70bc58a38f4880ff
eborszem <eborszem@purdue.edu>
2025-06-30 04:08:58 -0400

    vercel stuff fix

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Fretboard />} />
                 </Routes>
+                <Analytics />
             </div>
         </Router>
     );
