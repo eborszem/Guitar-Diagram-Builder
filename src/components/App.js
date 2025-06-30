7974c2974d8005cc342df0c982b012fcacb8f22f
eborszem <eborszem@purdue.edu>
2025-06-30 04:05:44 -0400

    vercel stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 /* client/src/components/App.js */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { Analytics } from "@vercel/analytics/react"
 import Fretboard from './Fretboard';
 import '../elements/App.css';
 
