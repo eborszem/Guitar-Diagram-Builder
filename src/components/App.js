548340fcffcd85005270ba8ef5eb10016d04d0ee
eborszem <eborszem@purdue.edu>
2025-07-06 00:57:04 -1000

    remove extra code

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 /* client/src/components/App.js */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/react"
 import Fretboard from './Fretboard';
 import '../elements/App.css';
 
@@ -12,7 +11,6 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Fretboard />} />
                 </Routes>
-                <Analytics />
             </div>
         </Router>
     );
